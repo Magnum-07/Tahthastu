353d7cc4f8ecb527f733e71e1ce62de0f8212139
Aditya Garg <66016411+Magnum-07@users.noreply.github.com>
2020-07-30 22:08:15 +0530

    Second commit

---
+++ Hello.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int i;
+int fun();
+
+int main()
+{
+
+    while(i)
+    {
+        fun();
+        main();
+    }
+    printf("hello\n");
+    return 0;
+}
+int fun()
+{
+    printf("Hi");
+}
